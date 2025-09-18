fe10b0779ae1cb48e656577c83a2710e66235015
Déborah Malheiro <malheirodev@gmail.com>
2025-09-18 20:20:54 +0000

    feat: concept of observable patterns

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -2,22 +2,41 @@ type SetterFn<T> = (prevState: T) => Partial<T>;
 
 export function createStore<TState>(initialState: TState) {
   let state = initialState;
+  const listeners = new Set<() => void>();
+
+  function subscribe(listener: () => void) {
+    listeners.add(listener);
+
+    return () => {
+      listeners.delete(listener);
+    };
+  }
+
+  function nofityListeners() {
+    listeners.forEach((listener) => listener());
+  }
 
   function setState(partialState: Partial<TState> | SetterFn<TState>) {
     const newValue =
       typeof partialState === 'function' ? partialState(state) : partialState;
 
     state = { ...state, ...newValue };
+
+    nofityListeners();
   }
 
   function getState() {
     return state;
   }
 
-  return { getState, setState };
+  return { getState, setState, subscribe };
 }
 
 const store = createStore({ userName: '', active: false, counter: 1 });
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
 store.setState({ userName: 'Debs' });
 store.setState((prevState) => ({ counter: prevState.counter + 1 }));
